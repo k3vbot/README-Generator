6f23529831b7819dd40016667c28072fa4b10650
Kevin <muehl004@gmail.com>
2022-07-08 19:48:22 -0700

    test

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,13 +3,16 @@
 function renderLicenseBadge(license) {
   switch (license) {
     case 'MIT':
-      return `[![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      //break;
     case 'Eclipse 1.0':
-      return `[![license](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+      return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+      //break;
     case 'Apache 2.0':
-      return `[![license](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      //break;
     case 'Perl':
-      return `[![license: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`;
+      return '[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)';
   }
 }
 
